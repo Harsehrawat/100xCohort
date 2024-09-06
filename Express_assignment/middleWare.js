3bd8bd957084d75c46cbc49dc2f1104ad7f532d6
Harsh Sehrawat <harsehrawat@gmail.com>
2024-09-06 13:46:41 +0530

    removed comments

---
+++ Express_assignment/middleWare.js
@@ -0,0 +1,26 @@
+// Create a middleware function that logs each incoming request’s HTTP method, URL, and timestamp to the console
+
+const express = require('express');
+const app = express();
+
+function logMethod(req,res,next){
+    console.log("Method of request is : "+ req.method);
+
+    next();
+}
+function logURL(req,res,next){
+    console.log("URL of current request is : "+req.originalUrl);
+
+    next();
+}
+function logTimeStamp(req,res){
+    console.log("current timestamp is : "+Date().toString());
+}
+
+app.get('/assignment', logMethod, logURL, logTimeStamp, (req, res) => {
+    res.send("Middleware logging complete!");
+});
+
+app.listen(4000, () => {
+    console.log('Server is running on port 3000');
+});
