54893cf0a219b8b965a5cdf0d4d8efca55e0f5f3
Harsh Sehrawat <harsehrawat@gmail.com>
2025-01-11 13:39:21 +0530

    Lesson : 1 updated

--- week_10/Day-2 Context API and Rolling Up the State/state_experiment/my-app/src/App.jsx
+++ week_10/Day-2 Context API and Rolling Up the State/state_experiment/my-app/src/App.jsx
@@ -1,31 +1,49 @@
-import { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { use } from 'react';
+
+const BulbContext = React.createContext();
+function BulbProvider ({children}){
+  const [bulbOn,setbulbOn] = useState(true);
+
+  return <BulbContext.Provider value={{
+    bulbOn : bulbOn,
+    setbulbOn : setbulbOn
+  }}>
+    {children}  
+    {/* // return the children with values set in Provider */}
+  </BulbContext.Provider>
+}
 
 function App() {
+  const [bulbOn,setbulbOn] = useState(true);
+
   return <div>
-    <LightBulb/> 
+    <BulbProvider>
+      <LightBulb/>
+    </BulbProvider>
   </div>  
 }
 
 function LightBulb(){
-  const [bulbOn,setbulbOn] = useState(true);  // use it here as it's the LCA for the components used in this project.
-
   return <div>
-    <BulbState bulbOn={bulbOn}/>
-    <ToggleBulb setbulbOn = {setbulbOn}/>
+    <BulbState />
+    <ToggleBulb  />
   </div>
 }
 
-function BulbState( {bulbOn}){
+function BulbState(){
+  const {bulbOn} = useContext(BulbContext);
   return <div>
     {bulbOn ?  "Bulb on" : "Bulb off"}
   </div>
 }
 
 
-function ToggleBulb({setbulbOn}){
+function ToggleBulb(){
+  const {setbulbOn} = useContext(BulbContext);
   return <div>
     <button onClick={ ()=>{ setbulbOn(c=>!c) }}>Toggle</button>
   </div>
