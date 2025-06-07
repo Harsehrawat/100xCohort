3fb439b726089a9af26d59ff61f9f50d064d7b57
Harsh Sehrawat <harsehrawat@gmail.com>
2025-06-07 15:34:24 +0530

    added SSG

--- NextJS Week 20/my-app/app/page.tsx
+++ NextJS Week 20/my-app/app/page.tsx
@@ -1,7 +1,7 @@
-import Image from "next/image";
+export const dynamic = "force-static";
 
 export default function Home() {
   return (
-    <div> Hi There</div>
+    <div>Hi There, SSG</div>
   )
 }
