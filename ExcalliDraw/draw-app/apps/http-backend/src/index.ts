4d7573d8b8661abe086540703db1a953bf4ab6a7
Harsh Sehrawat <harsehrawat@gmail.com>
2025-07-03 23:42:22 +0530

    added signin api

--- ExcalliDraw/draw-app/apps/http-backend/src/index.ts
+++ ExcalliDraw/draw-app/apps/http-backend/src/index.ts
@@ -2,20 +2,35 @@ import express from "express";
 import jwt from "jsonwebtoken";
 import {JWT_SECRET} from "@repo/backend-common/config";
 import {CreateUserSchema , SigninSchema, CreateRoomSchema} from "@repo/common/types";
+import {prismaClient} from "@repo/db/client";
 
 const app = express();
 
-app.post("/api/signup",(req,res)=>{
+app.post("/api/signup",async (req,res)=>{
     const username = req.body.username;
     const password = req.body.password;
 
     // verify username from the Database /
-    const data = CreateUserSchema.safeParse(req.body);
-    if(!data.success){
+    const parsedData = CreateUserSchema.safeParse(req.body);
+    if(!parsedData.success){
         res.json({
             message : "Incorrect input"
         })
         return;
+    }
+    
+    try{
+        await prismaClient.user.create({
+            data: {
+                email: parsedData.data?.username,
+                password: parsedData.data?.password,
+                name: parsedData.data?.name
+            }
+        })
+    }catch(e){
+        res.status(411).json({
+            message: "this username is already taken"
+        })   
     }
     
 
