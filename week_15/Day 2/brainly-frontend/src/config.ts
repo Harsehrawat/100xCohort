93d541fc26126d9c67ff37632f5049f426905ad5
harsh <harsh@Mac.lan>
2025-09-19 15:42:40 +0530

    updated Backend_URL

--- week_15/Day 2/brainly-frontend/src/config.ts
+++ week_15/Day 2/brainly-frontend/src/config.ts
@@ -1,4 +1,2 @@
-
-export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL
-
-export const FRONTEND_URL = "http://localhost:5173"
+export const BACKEND_URL = "http://localhost:3000";
+export const FRONTEND_URL = "http://localhost:5173";
